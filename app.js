d25779c18d3085f91da736603d8238ddd41468f8
lochanathisumgit-code <lochanathisumgit@gmail.com>
2026-09-25 12:07:36 +0530

    Dom manupulation

---
+++ app.js
@@ -0,0 +1,68 @@
+// class customer {
+//     name;
+//     age;
+//     address;
+
+//     constructor(name, age, address) {
+//         this.name = name;
+//         this.age = age;
+//         this.address = address;
+//     }   
+
+// }
+
+// let customer1 = new customer("John Doe", 30, "123 Main St");
+// console.log(customer1);
+// let customer2 = new customer("Jane Smith", 25, "456 Elm St");
+// console.log(customer2);
+
+// let student = [
+//     {
+//         name: "Alice Johnson",
+//         age: 20,
+//         address: "789 Oak St",
+//         teachers: [
+//             {
+//                 name: "Mr. Smith",
+//                 age: 40,
+//                 address: "654 Maple St"
+//             },
+//             {
+//                 name: "Ms. Johnson",
+//                 age: 35,
+//                 address: "987 Cedar St"
+//             }
+//         ],
+//         marks: [
+//             {
+//                 subject: "Math",
+//                 score: 95}
+//             ,
+//             {
+//                 subject: "Science",
+//                 score: 88}
+//             ,
+//             {
+//                 subject: "English",
+//                 score: 92}
+//         ]
+
+
+
+//     },
+//     {
+//         name: "Bob Brown",
+//         age: 22,
+//         address: "321 Pine St"
+//     }       
+
+// ]
+
+// //console.log(student);
+// console.log(student[0].teachers);
+// console.log(student[0].marks[1].subject);
+
+
+//-------------DOM manupulation-----------------
+console.log(document);
+document.write("Hello, World!");
